4692fa758e1c48e66b9241d789b1732f79151a26
Xin Niu <michaelniki1988@gmail.com>
2022-12-10 23:57:20 -0600

    Create cpp_debug_note.cpp

---
+++ cpp_debug_note.cpp
@@ -0,0 +1,10 @@
+main () {
+    // min() must take same data type for x and y (LC 275):
+    int num = citations.size() - right;
+    return min(num, citations[right]);
+    
+    // change data type on the run (LC 1838):
+    x = (long) nums[j] * (j - i + 1))
+  } 
+
+
